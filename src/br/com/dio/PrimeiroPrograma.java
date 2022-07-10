6b97d4042bf39893fab02f27abefdf6f4a3c733a
Brunna Godinho Gomes <bgodinhogomes@gmail.com>
2022-07-10 20:55:48 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+
+        System.out.println(cachorro);
+
+        /*int a = 2;
+        int b = 3;
+        System.out.println("Hello World!" + (a + b));*/
+    }
+}
